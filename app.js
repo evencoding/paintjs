2ef3f11541fe5053ca398acaef85ec7eb7fbb3a3
evencoding <88191233+evencoding@users.noreply.github.com>
2021-08-27 02:33:34 +0900

    # 2021-08-27

---
+++ app.js
@@ -0,0 +1,44 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+// canvas size
+canvas.width = 1100;
+canvas.height = 600;
+
+// Default Color
+ctx.strokeStyle = "#2c2c2c";
+// Default lineWidth
+ctx.lineWidh = 2.5;
+
+let painting = false;
+
+function stopPainting(event) {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
